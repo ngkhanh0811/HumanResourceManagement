8bf7c04d8c7e9c8eb5614731f8dcd245989c0abe
nguyenkhanh08112003 <91303308+nguyenkhanh08112003@users.noreply.github.com>
2023-03-09 13:43:45 +0700

    finish

---
+++ HumanResource/HumanResource/Data/HumanResourceContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HumanResource;
+
+namespace HumanResource.Data
+{
+    public class HumanResourceContext : DbContext
+    {
+        public HumanResourceContext (DbContextOptions<HumanResourceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HumanResource.Employee> Employee { get; set; } = default!;
+    }
+}
